a7cf0f6341345d59f90477b85a8c9793b6061b24
nassima <toumi.nassi@gmail.com>
2021-07-29 17:08:36 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    html = "<h3>Hello world ! This is Nassima ! </h3>"
+    return html.format(format)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
