25bf1553e31fd18c5e743b41c13c4df58581539b
nassima <toumi.nassi@gmail.com>
2021-07-29 17:32:30 +0200

    Introducing lint error

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    html = "<h3>Hello world ! This is Nassima ! </h3abc>"
+    html = "<h3>Hello world ! This is Nassima ! </h3>"
     return html.format(format)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+
+thisisthelinterror
